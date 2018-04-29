683e45b9aeb4fe585677827af336b2715f888e51
Papatiara <shirazipatricia@gmail.com>
2018-04-28 20:12:33 -0700

    passed all tests for tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,36 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-var treeMethods = {};
+ 
+var treeMethods = {}
 
 treeMethods.addChild = function(value) {
+  var newTree = Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
-};
+  var wasFound = false;
+  var rootNode = this;
 
+  var findTarget = function(currNode){
+    if (currNode.value === target) {
+      wasFound = true;
+    } else {
+      for (var i = 0; i < currNode.children.length; i++) {
+        findTarget(currNode.children[i]);
+      }
+    }
+  }
+  
+  findTarget(rootNode);
+  return wasFound
+};
 
 
 /*
