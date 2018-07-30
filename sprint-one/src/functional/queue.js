dc41fe532bd2412d3a49fe836f3ed4751603e281
Papatiara <shirazipatricia@gmail.com>
2018-07-30 09:26:44 -0700

    passing tests for queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,36 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  let size = 0;
 
   someInstance.enqueue = function(value) {
+    let count = size;
+    let keys = Object.keys(storage).map(elem => Number(elem));
+    if (keys.includes(size)) {
+      count++;
+      storage[count] = value;
+    } else {
+      storage[size] = value;
+    }
+    size++;
   };
 
   someInstance.dequeue = function() {
+    let keys = Object.keys(storage).map(elem => Number(elem));
+    let min = Math.min(...keys);
+    let item = storage[min];
+    delete storage[min];
+    console.log(storage);
+    if (size > 0) {
+      size--;
+    } else {
+      size = 0;
+    }
+    return item;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
