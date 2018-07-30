b90ff3ee65d505b3f3f7289d778ec98b335ed1ea
Papatiara <shirazipatricia@gmail.com>
2018-07-29 20:15:38 -0700

    passing tests stack;

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,25 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  let size = 0;
+  
   someInstance.push = function(value) {
+    storage[size++] = value;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      size--;
+    } else {
+      size = 0;
+    }
+    let item = storage[size];
+    delete storage[size];
+    return item;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
